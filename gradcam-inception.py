9f58fbc990b22c509a8def956b9205412ae31cd4
lennijusten <65619456+lennijusten@users.noreply.github.com>
2021-07-23 16:54:09 +0200

    Clean up

--- gradcam-inception.py
+++ gradcam-inception.py
@@ -10,46 +10,15 @@ import cv2
 
 species = ['Amblyomma', 'Dermacentor', 'Ixodes']
 
-model = keras.models.load_model('/Users/Lenni/Downloads/my_model')
+model = keras.models.load_model('my_model')
 
-img_path = '/Users/Lenni/Desktop/Screen Shot 2020-10-11 at 5.33.57 PM.png'
-# base_model = InceptionV3(
-#         input_shape=(224,224,3),
-#         include_top=False)
-# x = base_model.output
-# x1 = tf.keras.layers.GlobalMaxPooling2D()(x)
-# x2 = tf.keras.layers.Dense(3, activation='softmax')(x1)
-# model = tf.keras.models.Model(inputs=base_model.input, outputs=x2)
-#
-# model.load_weights('/Users/Lenni/Downloads/model_0.003.h5')
-
-# def loadModel():  # Load neural network with saved weights and architecture
-#     json_path =  '/Users/Lenni/Downloads/model_0.003.json' # look for any file with .json extension
-#     weights_path = '/Users/Lenni/Downloads/model_0.003.h5'  # look for any file with .h5 extension
-#
-#
-#     json_file = open(json_path, 'r')  # load first model instance
-#     loaded_model_json = json_file.read()
-#     json_file.close()
-#
-#     loaded_model = model_from_json(loaded_model_json)
-#     loaded_model.load_weights(weights_path)  # load first weights instance
-#
-#     print("Model loaded from disk.")
-#     # logging.info("loadModel() -- Success")
-#     return loaded_model
-#
-# model = loadModel()
-
-means = [0.485, 0.456, 0.406]
-std = 0.225
+img_path = 'image.png'
 
 img = Image.open(img_path)
 img_resized = img.resize((224, 224))
 pixels = np.asarray(img_resized)  # convert image to array
 pixels = pixels.astype('float32')
-# pixels /= 255.0
-# pixels = (pixels - means)/std
+
 input = np.expand_dims(pixels, axis=0)
 
 preds = model.predict(input)
@@ -64,7 +33,6 @@ with tf.GradientTape() as tape:
         pooled_grads = K.mean(grads, axis=(0, 1, 2))
 
 heatmap = tf.reduce_mean(tf.multiply(pooled_grads, last_conv_layer), axis=-1)
-
 
 heatmap = np.maximum(heatmap, 0)
 heatmap /= np.max(heatmap)
